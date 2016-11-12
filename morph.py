261f607a8f2270b30963723420a44b2e467c21fd
Anton Kondyurin <a.kondjurin@gmail.com>
2016-11-12 17:55:19 +0300

    добавил морф

--- morph.py
+++ morph.py
@@ -1,3 +1,15 @@
 import pymorphy2
+import re
+from db import db_session, Message
 
 m = pymorphy2.MorphAnalyzer()
+
+def split_message():
+    #взять из базы одну запись из которой вытащить message
+    text = db_session.query(Message).filter(Message.user_id == '1')
+    regex = re.compile('[^\w\d\s\-\n\.\(\)\{\}\[\]\"\'«»`%,:_]')
+    for message in text.all():
+        result = regex.findall(string, pos=None, endpos=None)
+    return print(result)
+
+split_message()
--- server.py
+++ server.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import db_session, Message
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    posts = db_session.query(Message).all()
+    user_id = request.args.get("user_id")
+    posts = db_session.query(Message)
+    if user_id:
+        posts = posts.filter(Message.user_id.like("%{}%".format(user_id)))
 
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', posts=posts.all())
 
 if __name__ ==  '__main__':
     app.run(port = 5020, debug = True)
